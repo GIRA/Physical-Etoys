e28191a31682ff14df04abb118a2146e79ecd552
caeti <caeti@caeti.(none)>
2013-01-02 14:02:05 -0300

    Added a third step in "build-release.sh" that would set permissions to all the executable files.

--- release/build-release.sh
+++ release/build-release.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 cd build
+
+# Create contents directory and move image, changes and sources
 mkdir contents
 mv *.image contents/pe.image
 mv *.changes contents/pe.changes
 mv *.stc contents
 
+# Execute PE release process
 EXE="$PWD/vm"
 RES="$PWD/contents"
-
-
-exec "$EXE/squeakvm" \
+"$EXE/squeakvm" \
 	-plugins "$EXE" \
 	-encoding latin1 \
-	-vm-display-X11  \
+	-vm-display-X11 \
 	"$RES/pe.image" \
 	"$PWD/../build-release.st"
+
+# Change permissions of executable files
+chmod ugo+x "$RES/Arduino/Sketches/upload.sh"
+chmod ugo+x "$RES/Arduino/hardware/tools/avr/bin/avrdude"
+chmod ugo+x "$RES/Arduino/hardware/tools/hid_bootcli/v1.0/hid_bootloader_cli.Linux"
