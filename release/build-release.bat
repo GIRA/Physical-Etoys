23d143f73abc780fdf93a4e0c6250699ff430eb9
Ricardo Moran <richi.moran@gmail.com>
2013-01-07 16:41:59 -0300

    Updated build-release.bat to move the image, changes and sources into the "contents" directory, like the unix script does.

--- release/build-release.bat
+++ release/build-release.bat
@@ -1,2 +1,14 @@
 cd build
-@start PhysicalEtoys.exe *.image "..\build-release.st" %1
+
+REM Create contents directory and move image, changes and sources
+mkdir contents
+mv *.image contents/pe.image
+mv *.changes contents/pe.changes
+mv *.stc contents
+mv *.README contents
+
+REM Execute PE release process
+REM @start PhysicalEtoys.exe "%CD%\contents\pe.image" "..\build-release.st" %1
+@start PhysicalEtoys.exe "%CD%\..\build-release.st" %1
+
+cd ..
