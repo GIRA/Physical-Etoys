140de57d1cec87565f79fab993ae17f62c771127
Ricardo Moran <richi.moran@gmail.com>
2013-01-18 15:22:35 -0300

    Changed build-release.bat to be more compatible with the unix version regarding the arguments

--- release/build-release.bat
+++ release/build-release.bat
@@ -1,3 +1,9 @@
+REM Get arguments
+set BUILDER=
+if "%1" == "-b" set BUILDER=%2
+if "%1" == "--builder" set BUILDER=%2
+
+REM Change current directory to build
 cd build
 
 REM Create contents directory and move image, changes and sources
@@ -8,6 +14,7 @@ mv *.stc contents
 mv *.README contents
 
 REM Execute PE release process
-@start PhysicalEtoys.exe "%CD%\..\build-release.st" %1
+@start PhysicalEtoys.exe "%CD%\..\build-release.st" %BUILDER%
 
+REM Change current directory back to release
 cd ..
