4acddf916885055445e2f5cbcfaf769c41f69cb0
Ricardo Moran <richi.moran@gmail.com>
2013-02-07 17:37:05 -0300

    Removed comment

--- release/build-release.bat
+++ release/build-release.bat
@@ -8,7 +8,6 @@ mv *.stc contents
 mv *.README contents
 
 REM Execute PE release process
-REM @start PhysicalEtoys.exe "%CD%\contents\pe.image" "..\build-release.st" %1
 @start PhysicalEtoys.exe "%CD%\..\build-release.st" %1
 
 cd ..
